f2f920ccd1999db8c654bbdef35dd48ea9490ad8
Austin Jenchi <ajenchi9@gmail.com>
2020-09-20 04:22:55 -0700

    add loading indicators and no card text

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import firebase from 'firebase/app';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 export interface CardType {
   content: string;
@@ -13,11 +14,11 @@ interface Props {
   component: React.ElementType
 }
 
+const initialCardState: CardContents[] | null = null;
+
 function CardList(props: Props) {
   const Tag: React.ElementType = props.component;
-  const [cards, setCards] = useState([] as CardContents[]);
-  const cardElements = cards.map((value, idx) => <Tag key={idx} index={idx} title={value.title} content={value.message} />);
-  
+  const [cards, setCards] = useState(initialCardState);
   useEffect(() => {
     const dbRef = firebase.database().ref('posts');
     dbRef.on('value', snapshot => {
@@ -28,11 +29,20 @@ function CardList(props: Props) {
     }
   }, []);
 
-  return (
-    <>
-      {cardElements}
-    </>
-  );
+  if (cards === null) {
+    return <CircularProgress />
+  } else {
+    if (cards.length === 0) {
+      return <p className="status-center">Nothing's here yet!</p>
+    } else {
+      const cardElements = cards.map((value, idx) => <Tag key={idx} index={idx} title={value.title} content={value.message} />);
+      return (
+        <>
+          {cardElements}
+        </>
+      );
+    }
+  }
 }
 
 export default CardList;
